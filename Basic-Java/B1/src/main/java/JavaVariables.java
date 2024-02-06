618d331756c8457ddc3ac1d689868fd6ba965be9
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-02-07 03:28:35 +0600

    Variables in Java Programming

---
+++ Basic-Java/B1/src/main/java/JavaVariables.java
@@ -0,0 +1,26 @@
+public class JavaVariables {
+    public static void main(String[] args) {
+        // Declare and initialize a variable of type int
+        int myNumber = 10; // This variable stores an integer value
+
+        // Declare a variable without initializing it
+        double myDouble; // This variable will store a double value
+
+        // Assign a value to the previously declared variable
+        myDouble = 55.5; // Now myDouble stores a double value
+
+        // Declare and initialize a variable of type boolean
+        boolean isJavaFun = true; // This variable stores a boolean value
+        boolean isNahin = false; // False method of boolean
+
+        // Declare and initialize a variable of type String
+        String greeting = "Hello, Java!"; // This variable stores a string value
+
+        // Output the values stored in the variables
+        System.out.println("myNumber: " + myNumber); // Output: myNumber: 10
+        System.out.println("myDouble: " + myDouble); // Output: myDouble: 5.5
+        System.out.println("isJavaFun: " + isJavaFun); // Output: isJavaFun: true
+        System.out.println("Nahin is 23 years old: " + isNahin); // Output: Nahin is 23 years old: false
+        System.out.println("greeting: " + greeting); // Output: greeting: Hello, Java!
+    }
+}
