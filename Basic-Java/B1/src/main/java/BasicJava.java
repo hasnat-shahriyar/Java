8f63d4c257fe65ca3c003cc6aff210b87daf7b15
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-02-07 02:39:42 +0600

    print output method and comment in java

---
+++ Basic-Java/B1/src/main/java/BasicJava.java
@@ -0,0 +1,10 @@
+public class BasicJava {
+    //Two ways to print output
+    public static void main (String[] args){
+//        Method 1 : Print Line "println"
+        System.out.println("Hello World");
+//        Method 2 : Print Method "print"
+        System.out.print("Hasnat ");
+        System.out.print("Shahriyar");
+    }
+}
